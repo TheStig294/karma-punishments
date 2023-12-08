5aab473d014bbe0db2106296781fd8a64bd4bebf
The Stig <thestig294@gmail.com>
2023-12-08 12:28:19 +0000

    Fixed reverse punishment

--- lua/karma_punishments/punishments/reverse.lua
+++ lua/karma_punishments/punishments/reverse.lua
@@ -9,28 +9,30 @@ PUNISHMENT.extDesc = "Reverses many controls like moving backwards,\nshooting <-
 function PUNISHMENT:Apply(ply)
     ply:SetLadderClimbSpeed(-200)
 
-    self:AddHook("StartCommand", function(p, cmd)
-        if not self:IsPunishedPlayer(p) then return end
-        -- Make the player move the opposite direction
-        cmd:SetForwardMove(-cmd:GetForwardMove())
-        cmd:SetSideMove(-cmd:GetSideMove())
+    if CLIENT then
+        self:AddHook("StartCommand", function(p, cmd)
+            if not self:IsPunishedPlayer(p) then return end
+            -- Make the player move the opposite direction
+            cmd:SetForwardMove(-cmd:GetForwardMove())
+            cmd:SetSideMove(-cmd:GetSideMove())
 
-        -- Attack reloads, reload attacks
-        if cmd:KeyDown(IN_ATTACK) then
-            cmd:RemoveKey(IN_ATTACK)
-            cmd:SetButtons(cmd:GetButtons() + IN_RELOAD)
-        elseif cmd:KeyDown(IN_RELOAD) then
-            cmd:RemoveKey(IN_RELOAD)
-            cmd:SetButtons(cmd:GetButtons() + IN_ATTACK)
-        elseif cmd:KeyDown(IN_JUMP) then
-            -- Reverse jump and duck too
-            cmd:RemoveKey(IN_JUMP)
-            cmd:SetButtons(cmd:GetButtons() + IN_DUCK)
-        elseif cmd:KeyDown(IN_DUCK) then
-            cmd:RemoveKey(IN_DUCK)
-            cmd:SetButtons(cmd:GetButtons() + IN_JUMP)
-        end
-    end)
+            -- Attack reloads, reload attacks
+            if cmd:KeyDown(IN_ATTACK) then
+                cmd:RemoveKey(IN_ATTACK)
+                cmd:SetButtons(cmd:GetButtons() + IN_RELOAD)
+            elseif cmd:KeyDown(IN_RELOAD) then
+                cmd:RemoveKey(IN_RELOAD)
+                cmd:SetButtons(cmd:GetButtons() + IN_ATTACK)
+            elseif cmd:KeyDown(IN_JUMP) then
+                -- Reverse jump and duck too
+                cmd:RemoveKey(IN_JUMP)
+                cmd:SetButtons(cmd:GetButtons() + IN_DUCK)
+            elseif cmd:KeyDown(IN_DUCK) then
+                cmd:RemoveKey(IN_DUCK)
+                cmd:SetButtons(cmd:GetButtons() + IN_JUMP)
+            end
+        end)
+    end
 
     -- Override the sprint key so living players can sprint forward while holding the back key
     self:AddHook("TTTSprintKey", function(p)

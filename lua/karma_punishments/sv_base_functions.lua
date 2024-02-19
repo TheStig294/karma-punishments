b1e110dd14622ff8af8acf038e01ab750a57ff63
The Stig <thestig294@gmail.com>
2024-02-19 23:15:27 +1030

    ply -> victim

--- lua/karma_punishments/sv_base_functions.lua
+++ lua/karma_punishments/sv_base_functions.lua
@@ -2,12 +2,12 @@
 -- Server-side karma punishment functions 
 -- 
 -- Debug command for testing punishments, only works on a peer-to-peer server for the server host if sv_cheats is on
-concommand.Add("kp_apply", function(ply, _, args, argsStr)
+concommand.Add("kp_apply", function(victim, _, args, argsStr)
     if argsStr ~= "" then
         local PUNISHMENT = TTTKP.punishments[args[1]]
-        TTTKP:ApplyPunishment(ply, PUNISHMENT)
+        TTTKP:ApplyPunishment(victim, PUNISHMENT)
     else
-        TTTKP:SelectPunishment(ply)
+        TTTKP:SelectPunishment(victim)
     end
 end, nil, "Applies a karma punishment to yourself, if no argument is given, selects a random one", FCVAR_CHEAT)
 
@@ -33,49 +33,49 @@ function TTTKP:IsValidPunishment(PUNISHMENT, victim)
 end
 
 -- NOTE: Can return nil if all punishments are disabled! (Or conditions aren't met)
-function TTTKP:GetValidPunishment(ply)
+function TTTKP:GetValidPunishment(victim)
     for id, PUNISHMENT in RandomPairs(TTTKP.punishments) do
-        if TTTKP:IsValidPunishment(PUNISHMENT, ply) then return PUNISHMENT end
+        if TTTKP:IsValidPunishment(PUNISHMENT, victim) then return PUNISHMENT end
     end
 end
 
 -- Finds a punishment for the player that can be applied, and applies it with TTTKP:ApplyPunishment()
 -- Returns true if no punishment was actually applied
-function TTTKP:SelectPunishment(ply, karmaPercentMessage)
-    if not IsValid(ply) or not ply:IsPlayer() then return end
+function TTTKP:SelectPunishment(victim, karmaPercentMessage)
+    if not IsValid(victim) or not victim:IsPlayer() then return end
     -- Choose a random punishment from available ones to give to the player
-    local PUNISHMENT = TTTKP:GetValidPunishment(ply)
+    local PUNISHMENT = TTTKP:GetValidPunishment(victim)
 
     -- If all punishments are turned off or conditions not met then do nothing
     if not PUNISHMENT then
-        ply:ChatPrint("You should have received a karma punishment, but none are enabled with its condition met...")
+        victim:ChatPrint("You should have received a karma punishment, but none are enabled with its condition met...")
 
         return true
     end
 
-    hook.Run("TTTKPSelected", ply, PUNISHMENT)
-    TTTKP:ApplyPunishment(ply, PUNISHMENT, karmaPercentMessage)
+    hook.Run("TTTKPSelected", victim, PUNISHMENT)
+    TTTKP:ApplyPunishment(victim, PUNISHMENT, karmaPercentMessage)
 end
 
 -- Applies all punishment effects
 util.AddNetworkString("TTTKPApply")
 
-function TTTKP:ApplyPunishment(ply, PUNISHMENT, karmaPercentMessage)
-    if not IsValid(ply) or not ply:IsPlayer() then return end
+function TTTKP:ApplyPunishment(victim, PUNISHMENT, karmaPercentMessage)
+    if not IsValid(victim) or not victim:IsPlayer() then return end
 
     -- Accept string ids for punishments as well
     if isstring(PUNISHMENT) then
         PUNISHMENT = TTTKP.punishments[PUNISHMENT]
     end
 
-    if not TTTKP:IsValidPunishment(PUNISHMENT, ply) then return end
+    if not TTTKP:IsValidPunishment(PUNISHMENT, victim) then return end
     -- Punishment function (Where all the magic happens...)
-    PUNISHMENT:Apply(ply)
+    PUNISHMENT:Apply(victim)
     table.insert(TTTKP.activePunishments, PUNISHMENT)
     -- Add punishment table to the player object for easy reference
     -- Used for detecting if a player is punished
     PUNISHMENT.active = true
-    ply.KPPunishment = PUNISHMENT
+    victim.KPPunishment = PUNISHMENT
 
     -- Stops the "Karma below 80%" part of the alert chat message from appearing
     -- Used when manually applying punishments to players
@@ -87,7 +87,7 @@ function TTTKP:ApplyPunishment(ply, PUNISHMENT, karmaPercentMessage)
     net.Start("TTTKPApply")
     net.WriteString(PUNISHMENT.id)
     net.WriteBool(karmaPercentMessage)
-    net.Send(ply)
+    net.Send(victim)
 end
 
 -- Getting the low karma amount convar and ttt_karma convar
@@ -104,9 +104,9 @@ hook.Add("TTTBeginRound", "TTTKPApplyPunishments", function()
     if not karmaCvar:GetBool() then return end
 
     timer.Simple(0.1, function()
-        for _, ply in ipairs(player.GetAll()) do
-            if ply:GetLiveKarma() < thresholdCvar:GetInt() and ply:Alive() and not ply:IsSpec() then
-                TTTKP:SelectPunishment(ply, true)
+        for _, victim in ipairs(player.GetAll()) do
+            if victim:GetLiveKarma() < thresholdCvar:GetInt() and victim:Alive() and not victim:IsSpec() then
+                TTTKP:SelectPunishment(victim, true)
             end
         end
     end)
@@ -116,22 +116,22 @@ end)
 hook.Add("PlayerSay", "TTTKPManualPunishment", function(sender, text, teamChat)
     if not sender:IsAdmin() or not string.StartsWith(text, "/punish ") then return end
     local playerName = string.lower(string.sub(text, 9))
-    local playerToPunish
+    local victim
 
     for _, ply in ipairs(player.GetAll()) do
-        if string.lower(ply:Nick()) == playerName then
-            playerToPunish = ply
+        if string.lower(victim:Nick()) == playerName then
+            victim = ply
             break
         end
     end
 
-    if IsValid(playerToPunish) then
-        local noValidPunishment = TTTKP:SelectPunishment(playerToPunish)
+    if IsValid(victim) then
+        local noValidPunishment = TTTKP:SelectPunishment(victim)
 
         if noValidPunishment then
-            sender:ChatPrint(playerToPunish:Nick() .. " should have received a karma punishment, but none are enabled with its condition met...")
+            sender:ChatPrint(victim:Nick() .. " should have received a karma punishment, but none are enabled with its condition met...")
         else
-            sender:ChatPrint(playerToPunish:Nick() .. " was punished!")
+            sender:ChatPrint(victim:Nick() .. " was punished!")
         end
     else
         sender:ChatPrint("Player not found")
